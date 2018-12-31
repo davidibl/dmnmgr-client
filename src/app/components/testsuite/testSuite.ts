387204ea2ed4caf668231233a35a13c6b98848c8
David Ibl <david.ibl@xnoname.com>
2018-12-31 09:40:07 +0100

    first working implementation of testsuite

--- src/app/components/testsuite/testSuite.ts
+++ src/app/components/testsuite/testSuite.ts
@@ -2,10 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { TestSuiteService } from '../../services/testSuiteService';
 import { Observable } from 'rxjs/Observable';
 import { Testsuite } from '../../model/testsuite';
-import { map } from 'rxjs/operators';
+import { map } from 'rxjs/operators/map';
+import { take } from 'rxjs/operators/take';
 import { Test } from '../../model/test';
 import { ObjectDefinition } from '../../model/json/objectDefinition';
 import { DataModelService } from '../../services/dataModelService';
+import { TestDecisionService } from '../../services/testDecisionService';
 
 export class TestCaseContainer {
     public result: boolean;
@@ -21,12 +23,15 @@ export class TestSuiteComponent implements OnInit{
 
     public testCases$: Observable<TestCaseContainer[]>;
     public dataModel$: Observable<ObjectDefinition>;
+    public responseModel$: Observable<ObjectDefinition>;
 
     public constructor(private _testSuiteService: TestSuiteService,
-                       private _dataModelService: DataModelService) {}
+                       private _dataModelService: DataModelService,
+                       private _testDecisionService: TestDecisionService) {}
 
     public ngOnInit() {
         this.dataModel$ = this._dataModelService.getDataModel();
+        this.responseModel$ = this._dataModelService.getResponseModel();
         this.testCases$ = this._testSuiteService
             .getTestSuite()
             .pipe(
@@ -41,5 +46,12 @@ export class TestSuiteComponent implements OnInit{
 
     public deleteCase(item: TestCaseContainer) {
         this._testSuiteService.deleteTestCase(item.testcase);
+    }
+
+    public testCase(item: TestCaseContainer) {
+        this._testDecisionService.getTestResult()
+            .pipe( take(1) )
+            .subscribe(result => item.result = result['testSucceded']);
+        this._testDecisionService.testDecision(item.testcase);
     }
 }
