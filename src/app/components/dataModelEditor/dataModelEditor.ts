1d5117a991b1680c1556b36a5d8927889c29d6f8
David Ibl <david.ibl@xnoname.com>
2018-12-31 17:31:33 +0100

    response model gets synced with output columns

--- src/app/components/dataModelEditor/dataModelEditor.ts
+++ src/app/components/dataModelEditor/dataModelEditor.ts
@@ -1,18 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { EditorType } from '../../model/json/editorType';
 import { ObjectDefinition } from '../../model/json/objectDefinition';
 import { DataModelService } from '../../services/dataModelService';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
     selector: 'xn-data-model-editor',
     templateUrl: 'dataModelEditor.html',
     styleUrls: ['dataModelEditor.scss'],
 })
-export class DataModelEditorComponent {
+export class DataModelEditorComponent implements OnInit {
 
     public editorTypes = EditorType;
 
+    public responseModel$: Observable<ObjectDefinition>;
+
     public constructor(private _dataModelService: DataModelService) {}
+
+    public ngOnInit() {
+        this.responseModel$ = this._dataModelService.getResponseModel();
+    }
 
     public onRequestModelChanged(requestModel: ObjectDefinition) {
         this._dataModelService.newDataModel(requestModel);
