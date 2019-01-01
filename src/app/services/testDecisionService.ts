7cadd69a698ac288ba3b1f1480b8e65b0fa75b52
David Ibl <david.ibl@xnoname.com>
2019-01-01 15:39:27 +0100

    added artefact id to simulation and test to evaluate concrete decisions.

--- src/app/services/testDecisionService.ts
+++ src/app/services/testDecisionService.ts
@@ -9,13 +9,31 @@ import { ObjectDefinition } from '../model/json/objectDefinition';
 import { JsonDatatype } from '../model/json/jsonDatatypes';
 import { Test } from '../model/test';
 import { Observable } from 'rxjs';
+import { EventService } from './eventService';
+import { NewViewEvent } from '../model/newViewEvent';
+import { RenameArtefactEvent } from '../model/renameArtefactEvent';
+import { EventType } from '../model/eventType';
 
 @Injectable()
 export class TestDecisionService {
 
+    private _currentArtefactId: string;
+
     private _resultSubject = new ReplaySubject<DecisionSimulationResult>(1);
 
-    public constructor(private _http: HttpClient, private dmnXmlService: DmnXmlService) { }
+    public constructor(private _http: HttpClient,
+                       private dmnXmlService: DmnXmlService,
+                       private _eventService: EventService) {
+        this._eventService
+            .getEvent<NewViewEvent>((ev) => ev.type === EventType.NEW_VIEW)
+            .subscribe(event => {
+                this._currentArtefactId = event.data.artefactId;
+                this._resultSubject.next(null);
+            });
+        this._eventService
+            .getEvent<RenameArtefactEvent>((ev) => ev.type === EventType.RENAME_ARTEFACT)
+            .subscribe(event => this._currentArtefactId = event.data.artefactId);
+    }
 
     public simulateDecision(simulationData: Object) {
         this._resultSubject.next(null);
@@ -25,6 +43,7 @@ export class TestDecisionService {
             .pipe(
                 map(xml => {
                     return {
+                        dmnTableId: this._currentArtefactId,
                         variables: simulationData,
                         xml: xml
                     }
@@ -40,6 +59,7 @@ export class TestDecisionService {
             .pipe(
                 map(xml => {
                     return {
+                        dmnTableId: this._currentArtefactId,
                         variables: test.data,
                         expectedData: test.expectedData,
                         xml: xml
