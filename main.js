fbd13fd2688193a28172d41b81939cd1a2642f11
David Ibl <david.ibl@xnoname.com>
2019-01-03 19:52:50 +0100

    favicon and electron build

--- main.js
+++ main.js
@@ -1,52 +1,73 @@
-const { app, BrowserWindow } = require('electron')
-
-// Behalten Sie eine globale Referenz auf das Fensterobjekt.
-// Wenn Sie dies nicht tun, wird das Fenster automatisch geschlossen,
-// sobald das Objekt dem JavaScript-Garbagekollektor übergeben wird.
-
-let win
-
-function createWindow () {
-  // Erstellen des Browser-Fensters.
-  win = new BrowserWindow({ width: 800, height: 600 })
-
-  // und Laden der index.html der App.
-  win.loadFile('dist/index.html')
-
-  // Öffnen der DevTools.
-  win.webContents.openDevTools()
-
-  // Ausgegeben, wenn das Fenster geschlossen wird.
-  win.on('closed', () => {
-    // Dereferenzieren des Fensterobjekts, normalerweise würden Sie Fenster
-    // in einem Array speichern, falls Ihre App mehrere Fenster unterstützt.
-    // Das ist der Zeitpunkt, an dem Sie das zugehörige Element löschen sollten.
-    win = null
-  })
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var electron_1 = require("electron");
+var path = require("path");
+var url = require("url");
+var win, serve;
+var args = process.argv.slice(1);
+serve = args.some(function (val) { return val === '--serve'; });
+function createWindow() {
+    var electronScreen = electron_1.screen;
+    var size = electronScreen.getPrimaryDisplay().workAreaSize;
+    // Create the browser window.
+    win = new electron_1.BrowserWindow({
+        x: 0,
+        y: 0,
+        width: size.width,
+        height: size.height,
+        title: 'bpmgr',
+        webPreferences: {
+            devTools: false
+        }
+    });
+    if (serve) {
+        require('electron-reload')(__dirname, {
+            electron: require(__dirname + "/node_modules/electron")
+        });
+        win.loadURL('http://localhost:4200');
+        win.webContents.openDevTools();
+    }
+    else {
+        win.loadURL(url.format({
+            pathname: path.join(__dirname, 'dist/index.html'),
+            protocol: 'file:',
+            slashes: true
+        }));
+    }
+    // Emitted when the window is closed.
+    win.on('closed', function () {
+        // Dereference the window object, usually you would store window
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null;
+    });
 }
-
-// Diese Methode wird aufgerufen, wenn Electron mit der
-// Initialisierung fertig ist und Browserfenster erschaffen kann.
-// Einige APIs können nur nach dem Auftreten dieses Events genutzt werden.
-app.on('ready', createWindow)
-
-// Verlassen, wenn alle Fenster geschlossen sind.
-app.on('window-all-closed', () => {
-  // Unter macOS ist es üblich für Apps und ihre Menu Bar
-  // aktiv zu bleiben bis der Nutzer explizit mit Cmd + Q die App beendet.
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', () => {
-  // Unter macOS ist es üblich ein neues Fenster der App zu erstellen, wenn
-  // das Dock Icon angeklickt wird und keine anderen Fenster offen sind.
-  if (win === null) {
-    createWindow()
-  }
-})
-
-// In dieser Datei können Sie den Rest des App-spezifischen
-// Hauptprozess-Codes einbinden. Sie können den Code auch
-// auf mehrere Dateien aufteilen und diese hier einbinden.
+try {
+    // This method will be called when Electron has finished
+    // initialization and is ready to create browser windows.
+    // Some APIs can only be used after this event occurs.
+    electron_1.app.on('ready', createWindow);
+    // Quit when all windows are closed.
+    electron_1.app.on('window-all-closed', function () {
+        // On OS X it is common for applications and their menu bar
+        // to stay active until the user quits explicitly with Cmd + Q
+        if (process.platform !== 'darwin') {
+            electron_1.app.quit();
+        }
+    });
+    electron_1.app.on('browser-window-created', function (e, window) {
+        window.setMenu(null);
+    });
+    electron_1.app.on('activate', function () {
+        // On OS X it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (win === null) {
+            createWindow();
+        }
+    });
+}
+catch (e) {
+    // Catch Error
+    // throw e;
+}
+//# sourceMappingURL=main.js.map
