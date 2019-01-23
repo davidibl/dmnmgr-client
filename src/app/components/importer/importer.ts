11611da30dd5ee56cae7e0d528784ed90a1f3216
David Ibl <david.ibl@xnoname.com>
2019-01-23 22:02:52 +0100

    importer recognizes null data and separation chars are now configurable

--- src/app/components/importer/importer.ts
+++ src/app/components/importer/importer.ts
@@ -9,14 +9,27 @@ import { BaseEvent } from '../../model/event';
 })
 export class ImporterComponent {
 
+    private static SEMICOLON = ';';
+    private static NEWLINE_WINDWS = '\r\n';
+    private static NEWLINE_UNIX = '\n';
+
     public separatedData: string;
+    public fieldSeparator: string;
+    public recordSeparator: string;
+    public replaceAll = false;
 
     public constructor(private _eventService: EventService) {}
 
     public import() {
-        const rowSeparator = (this.separatedData.indexOf('\r\n') > -1) ? '\r\n' : '\n';
-        const rows = this.separatedData.split(rowSeparator);
-        const cols = rows.map(row => row.split(';'));
+        if (!this.separatedData) { return; }
+
+        const recordSeparator = (this.recordSeparator) ? this.recordSeparator :
+            (this.separatedData.indexOf(ImporterComponent.NEWLINE_WINDWS) > -1) ?
+                ImporterComponent.NEWLINE_WINDWS : ImporterComponent.NEWLINE_UNIX;
+        const fieldSeparator = (this.fieldSeparator) ? this.fieldSeparator : ImporterComponent.SEMICOLON;
+
+        const rows = this.separatedData.split(recordSeparator);
+        const cols = rows.map(row => row.split(fieldSeparator));
 
         const event = new BaseEvent<string[][]>('import', cols);
         this._eventService.publishEvent(event);
