518e47b8a01005e072ee576e4e2edfbcae5e99ae
David Ibl <david.ibl@xnoname.com>
2019-02-03 16:03:58 +0100

    save state service no storing the tye of unsaved changes

--- src/app/services/saveStateService.ts
+++ src/app/services/saveStateService.ts
@@ -1,21 +1,40 @@
 import { Injectable } from "@angular/core";
 import { EventService } from './eventService';
 import { EventType } from '../model/event/eventType';
+import { DataChangeTypes } from '../model/event/dataChangedType';
 
 @Injectable()
 export class SaveStateService {
 
-    private hasChange = false;
+    private _unsavedChanges: DataChangeTypes[] = [];
 
     public constructor(private _eventService: EventService) {
         this._eventService
             .getEvent((ev) => ev.type === EventType.DATA_CHANGED)
-            .subscribe(_ => {
-                this.hasChange = true;
+            .subscribe(ev => {
+                this.addToUnsavedChanges(ev.data);
             });
     }
 
+    public hasChanges() {
+        return this._unsavedChanges.length > 0;
+    }
+
     public resetChanges() {
-        this.hasChange = false;
+        this._unsavedChanges = [];
+    }
+
+    public resetChange(changeType: DataChangeTypes) {
+        this.removeFromUnsavedChanges(changeType);
+    }
+
+    private addToUnsavedChanges(changeType: DataChangeTypes) {
+        if (this._unsavedChanges.indexOf(changeType) < 0) {
+            this._unsavedChanges.push(changeType);
+        }
+    }
+
+    private removeFromUnsavedChanges(changeType: DataChangeTypes) {
+        this._unsavedChanges.splice(this._unsavedChanges.findIndex(type => type === changeType), 1);
     }
 }
