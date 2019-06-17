bd4139c9c5cee9fae33960960e8862d82cd7be40
David Ibl <david.ibl@xnoname.com>
2019-06-17 22:25:48 +0200

    improved huel recognition

--- src/app/model/dmn/dmnExpressionLanguage.ts
+++ src/app/model/dmn/dmnExpressionLanguage.ts
@@ -1,4 +1,16 @@
 export class DmnExpressionLanguage {
     static JUEL = 'juel';
     static FEEL = 'feel';
+
+    static isJuel(language: string) {
+        return this.isLanguage(language, DmnExpressionLanguage.JUEL);
+    }
+
+    static isFeel(language: string) {
+        return this.isLanguage(language, DmnExpressionLanguage.FEEL);
+    }
+
+    private static isLanguage(value: string, reference: string): boolean {
+        return !!value && value.toLowerCase() === reference;
+    }
 }
