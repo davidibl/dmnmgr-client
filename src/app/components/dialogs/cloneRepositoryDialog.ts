aa77a0dc2ebc467fdea0a57c8c29de6aabb2359d
David Ibl <david.ibl@xnoname.com>
2019-11-22 21:08:23 +0100

    FEAT: Clone Dialog resets after choosing to clone or cancelling

--- src/app/components/dialogs/cloneRepositoryDialog.ts
+++ src/app/components/dialogs/cloneRepositoryDialog.ts
@@ -1,9 +1,10 @@
-import { Component, ChangeDetectionStrategy, Output, EventEmitter, Input } from '@angular/core';
-import { WorkflowState, WorkflowStateTypes } from '@xnoname/web-components';
+import { Component, Output, EventEmitter, Input, ViewChildren, ViewChild, QueryList } from '@angular/core';
+import { WorkflowState, WorkflowStateTypes, AccordionComponent } from '@xnoname/web-components';
 import { CloneData } from '../../model/git/cloneData';
 import { FileService } from '../../services/fileService';
 import { filter } from 'rxjs/operators';
 import { FsResultType } from '../../model/fileSystemAccessResult';
+import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'xn-clone-repository-dialog',
@@ -12,10 +13,24 @@ import { FsResultType } from '../../model/fileSystemAccessResult';
 })
 export class CloneRepositoryDialogComponent {
 
+    private _open = false;
+
+    @ViewChildren(NgForm)
+    private _forms: QueryList<NgForm>;
+
+    @ViewChild(AccordionComponent)
+    private _accordion: AccordionComponent;
+
     public cloneData = new CloneData();
 
     @Input()
-    public open = false;
+    public set open(open: boolean) {
+        this._open = open;
+    }
+
+    public get open() {
+        return this._open;
+    }
 
     @Output()
     public openChange = new EventEmitter<boolean>();
@@ -27,7 +42,8 @@ export class CloneRepositoryDialogComponent {
 
     public onWorkflowStateChanged(workflowState: WorkflowState) {
         if (workflowState.state === WorkflowStateTypes.FINISHED) {
-            this.cloneDataComplete.emit(this.cloneData);
+            this.cloneDataComplete.emit(Object.assign({}, this.cloneData));
+            this.reset();
         }
     }
 
@@ -41,7 +57,13 @@ export class CloneRepositoryDialogComponent {
     }
 
     public onDialogOpenChanged(open: boolean) {
-        this.open = open;
+        this.reset();
+        this._open = open;
         this.openChange.emit(open);
+    }
+
+    public reset() {
+        this._forms.forEach(form => form.resetForm());
+        this._accordion.openStep(1);
     }
 }
