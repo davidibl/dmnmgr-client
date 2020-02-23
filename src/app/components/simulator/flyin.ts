0020be16105e9effdaca043cf6cc2917807c2910
David Ibl <david.ibl@xnoname.com>
2020-02-23 21:38:38 +0100

    FEAT: Flyin can now become wider with animation

--- src/app/components/simulator/flyin.ts
+++ src/app/components/simulator/flyin.ts
@@ -11,6 +11,12 @@ import { trigger, transition, style, animate, state } from '@angular/animations'
             state('*', style({ transform: 'translateX(-100%' })),
             transition('* => void', animate(300)),
             transition('void => *', animate(300))
+        ]),
+        trigger('wideFlyin', [
+            state('true', style({ width: '95%' })),
+            state('false', style({ width: 'inherit' })),
+            transition('true => false', animate(300)),
+            transition('false => true', animate(300))
         ])
     ]
 })
@@ -18,9 +24,20 @@ export class FlyinComponent {
 
     private _visible = false;
 
+    private _wideFlyin = false;
+
     @Input()
     public set visible(visible: boolean) {
         this._visible = visible;
+    }
+
+    @Input()
+    public set wideFlyin(wideFlyin: boolean) {
+        this._wideFlyin = wideFlyin;
+    }
+
+    public get wideFlyin() {
+        return this._wideFlyin;
     }
 
     @Output()
@@ -34,4 +51,5 @@ export class FlyinComponent {
         this._visible = false;
         this.visibleChange.emit(this._visible);
     }
+
 }
